82a5d945490d06ffa45427929000f2b6f4cd5ba0
Caio Queiroz <caiocqueiroz@github.com>
2025-04-01 15:23:54 -0300

    Teste push protection com segredo

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+  client_id       = "11111111-1111-1111-1111-111111111111"
+  client_secret   = "1234567890abcdef1234567890abcdef"  
+  tenant_id       = "22222222-2222-2222-2222-222222222222"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}

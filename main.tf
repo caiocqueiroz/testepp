b31e471ecbab69891a2e1075bebcc7a4a7e111c1
Caio Queiroz <caiocqueiroz@github.com>
2025-04-02 08:33:12 -0300

    asuhduahs

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 
   subscription_id = "00000000-0000-0000-0000-000000000000"
   client_id       = "11111111-1111-1111-1111-111111111111"
-  client_secret   = "1234567890abcdef1234567890abcdef"  
+  client_secret = "aBcDeFgHiJkLmNoPqRsTuVwXyZ1234567890abcd" 
   tenant_id       = "22222222-2222-2222-2222-222222222222"
 }
 
